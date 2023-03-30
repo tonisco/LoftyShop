3334061b2f2f38e25de649278f8bf1554557a78d
tonisco <tonyumeozulu@gmail.com>
2023-03-30 14:48:14 +0100

    dockerfile backend

--- backend/src/db/config.js
+++ backend/src/db/config.js
@@ -1,22 +1,17 @@
 import mongoose from "mongoose"
 
 const db = async () => {
-	try {
-		const connect = await mongoose.connect(
-			process.env.NODE_ENV === "production"
-				? process.env.DB_CONNECTION
-				: process.env.DB_CONNECTION_TEST,
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-				useCreateIndex: true,
-			}
-		)
-		console.log(`db connected on ${connect.connection.host}`)
-	} catch (err) {
-		console.error(`Error: ${err.message}`)
-		process.exit()
-	}
+    try {
+        const connect = await mongoose.connect(process.env.DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        console.log(`db connected on ${connect.connection.host}`)
+    } catch (err) {
+        console.error(`Error: ${err.message}`)
+        process.exit()
+    }
 }
 
 export default db
